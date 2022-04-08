870e78ec31ad2ab621d0404a15ba0d0222f0cdd2
Ginett Rosales <g.rosale@uniandes.edu.co>
2022-04-07 23:23:46 -0500

    Correcciones

--- application.py
+++ application.py
@@ -1,23 +1,24 @@
-from flaskr import create_app
-from .modelos import db, Blacklists, BlacklistsSchema, App_uuid
+from flask import Flask
+from config import create_app
+from modelos import db, Blacklists, BlacklistsSchema, App_uuid
 from flask_restful import Api  
-from .vistas import VistaBlacklists, VistaBlacklist, VistaTokenacceso
+from vistas import VistaBlacklists, VistaBlacklist, VistaTokenacceso
 from flask_jwt_extended import JWTManager
+import json
 
-
-app = create_app('default')
-app_context = app.app_context()
+application = create_app('default')
+app_context = application.app_context()
 app_context.push()
-
-db.init_app(app)
+db.init_app(application)
 db.create_all()
-
-api = Api(app)
+api = Api(application)
 api.add_resource(VistaBlacklists, '/blacklists')
 api.add_resource(VistaBlacklist, '/blacklist/<email>') ##/blacklists/<string:email>
-api.add_resource(VistaTokenacceso, '/tokenacceso')  
+api.add_resource(VistaTokenacceso, '/tokenacceso')
+jwt = JWTManager(application)
 
-jwt = JWTManager(app) 
+if __name__ == "__main__":
+    application.run(port = 5000, debug = True)
 
 ###PRUEBA PARA LA INSERTCION
 ##with app.app_context():
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from ..modelos import db, Blacklists, BlacklistsSchema
+from modelos import db, Blacklists, BlacklistsSchema
 from flask import request
 from flask_jwt_extended import jwt_required, create_access_token
 
